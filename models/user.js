4d0301efcf3e041b26fd93f2b2a1823d54d56c0d
Bradley Silcox <bradleysilcox80@gmail.com>
2025-03-30 12:20:06 -0700

    build: set up dependencies, file structure. feat: set up schemas for clothingItem and user models

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+});
+
+module.exports = ("user", userSchema);
