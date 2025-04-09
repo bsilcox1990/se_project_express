b240d6d22e9b34da0c210bc8c81446862b5431f9
Bradley Silcox <bradleysilcox80@gmail.com>
2025-04-09 13:23:15 -0700

    build: install helmet as dependency, use as middleware to set security headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
+const helmet = require("helmet");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -12,6 +13,7 @@ mongoose
   })
   .catch(console.error);
 
+app.use(helmet());
 app.use(express.json());
 app.use((req, res, next) => {
   req.user = {
